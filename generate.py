6dd234a9bdc7fd0ba9680d11a259b22d55b70444
Broclly <149013895+Broclly@users.noreply.github.com>
2025-02-23 03:04:27 -0500

    version 1.1
    added menu, replayability, made it easier to beat.

--- generate.py
+++ generate.py
@@ -8,34 +8,79 @@ import os
 
 minesweep_matrix = ["o","o","o","o","o","o","o","o","o","o",]
 cover_field = ["x","x","x","x","x","x","x","x","x","x"]
+mine_count = 0
 
 def generate_small():
     for i in range(4):
-        temp = random.randint(1,10)
-        minesweep_matrix.pop((temp-1))
-        minesweep_matrix.insert((temp-1),"*")
-
+        temp = random.randint(0,9)
+        minesweep_matrix.pop((temp))
+        minesweep_matrix.insert((temp),"*")
+    global mine_count
+    mine_count = minesweep_matrix.count("*")
 def sweeper_check(plot):
-    if minesweep_matrix[(plot - 1)] == "o":
-        return 0
+    if minesweep_matrix[(plot)] == "*":
+        return "1A"
     else:
-        return 1
+        temp = 0
+        try:
+            if minesweep_matrix[(plot + 1)] == "*":
+                temp += 1
+            if minesweep_matrix[(plot - 1)] == "*":
+                temp += 1
+            return temp
+        except:
+            return temp
+
 
 def field_update(plot,bomb):
-    cover_field.pop(plot-1)
-    if bomb == 1:
-        cover_field.insert((plot-1),"*")
+    cover_field.pop(plot)
+    if bomb == "1A":
+        cover_field.insert((plot),"*")
     else:
-        cover_field.insert((plot-1),"o")
+        if bomb == 0:
+            cover_field.insert((plot),"0")
+        else:
+            cover_field.insert((plot),str(bomb))
 
-def EOR_check(): 
-    if minesweep_matrix.count("o") == cover_field.count("o"):
+def EOR_check(): # Checks if you've won or lost
+    print(mine_count)
+    if cover_field.count("x") == mine_count: 
         os.system('cls')
+        UI_elements("sp")
         print("You win! Congratulations!")
-        time.sleep(4)
-        quit()
+        while True:
+            cont = input("Play again? (y/n)")
+            if cont == "y":
+                regenerate()
+                break
+            elif cont == "n":
+                print("See you later!")
+                time.sleep(4)
+                quit()
+            else:
+                print("Invalid answer!")  
     
     if cover_field.count("*") > 0:
         print("You landed on a bomb!! You lose!")
         time.sleep(4)
         quit()
+
+def regenerate(): # runs task to restart game
+    os.system('cls')
+    global cover_field
+    global minesweep_matrix
+    cover_field = ["x","x","x","x","x","x","x","x","x","x"]
+    minesweep_matrix = ["o","o","o","o","o","o","o","o","o","o",]
+    generate_small()
+
+def UI_elements(type): # basic hud
+    if type == "n": # displays current field
+        print("===================") 
+        print("Current Minefield:") 
+        print(cover_field) 
+        print("===================\n")
+    elif type == "sp": # displays final field
+        print("===================") 
+        print("Current Minefield:") 
+        print(minesweep_matrix) 
+        print("===================\n")
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import generate
 import os
 
 selection = 0
-build_ver = "1.1 (indev)"
+build_ver = "1.1"
 
 def welcome(): # intro message
     print("~Hello! Welcome to Minesweeper!~")
@@ -20,14 +20,18 @@ def start_menu(): # basic start CLI
         os.system('cls')
         print("Welcome! Pick your selection: ")
         print("1. Play!")
-        print("2. Quit.")
+        print("2. How to Play")
+        print("3. Quit.")
         selection = int(input("Pick an operation (enter the digit before)"))
         try:
-            if selection < 2 or selection > 0:
+            if selection < 3 or selection > 0:
                 if selection == 1:
                     gameplay()
                     return 0
                 elif selection == 2:
+                    os.system('cls')
+                    game_info()
+                elif selection == 3:
                     print("See you soon!")
                     time.sleep(1.5)
                     quit()
@@ -39,36 +43,41 @@ def start_menu(): # basic start CLI
             os.system('cls')
 
         
-
+def game_info():
+    print("Minesweeper works in a way where you guess spots that are safe, in order to find every possible safe spot.")
+    print("This specfic version uses 10 plots, starting at 0, and ending at 9. If a bomb is next to the spot you guessed \n it will be represented by the number of bombs adjacent.")
+    print("These are the basic rules of minesweeper!")
+    time.sleep(3)
+    temp = input("Ready to be sent back to the main menu? (y/n)")
+    if temp == "y":
+        os.system('cls')
+        start_menu()
+    else:
+        print("Alright then!")
+        time.sleep(4)
+        os.system('cls')
+        game_info()
 def gameplay(): # gameplay loop
     generate.generate_small() # generates minefield
 
     while True:
         os.system('cls')
-        UI_elements("n") 
+        generate.UI_elements("n") 
         try: # error
-            selection = int(input("Select a plot number (1-10): "))
+            selection = int(input("Select a plot number (0-9): "))
             status = generate.sweeper_check(selection)
             generate.field_update(selection,status)
         except: # handling.
             print("This is not a valid plot! Enter only numbers between 1 and 10 (inclusive)")
             time.sleep(3)
         os.system('cls')
-        UI_elements("n")
+        generate.UI_elements("n")
         generate.EOR_check()
         time.sleep(1)
 
-def UI_elements(type): # basic hud
-    if type == "n": # displays current field
-        print("===================") 
-        print("Current Minefield:") 
-        print(generate.cover_field) 
-        print("===================\n")
-    elif type == "sp": # displays final field
-        print("===================") 
-        print("Current Minefield:") 
-        print(generate.minesweep_matrix) 
-        print("===================\n")
+
+
+
 
 welcome()
 start_menu()
