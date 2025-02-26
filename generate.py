075995423df6cd762214eb25cc32838961386d2f
Broclly <149013895+Broclly@users.noreply.github.com>
2025-02-26 15:00:02 -0500

    added 1st turn immunity, and added more rows, levels, and mine display

--- generate.py
+++ generate.py
@@ -8,52 +8,82 @@ import os
 import colorama
 import player
 
-minesweep_matrix = ["o","o","o","o","o","o","o","o","o","o",]
-cover_field = ["x","x","x","x","x","x","x","x","x","x"]
+minesweep_matrix = [[]]
+cover_field = [[]]
 mine_count = 0
-player_data = player.Player()
+player_data = player.Player() 
+level = player_data.level
+total_x = 0
 
-def generate():
-    for i in range(4):
-        temp = random.randint(0,9)
-        minesweep_matrix.pop((temp))
-        minesweep_matrix.insert((temp),"*")
+
+def generate(): # geneerates a new field based on multiple parameters
     global mine_count
-    mine_count = minesweep_matrix.count("*")
-def sweeper_check(plot,turn):
-    if minesweep_matrix[(plot)] == "*" and turn > 0:
+    mine_count = 0
+    for x in range(level + 1): # generates 1 row, plus 1 for every level completed
+        minesweep_matrix.insert(x,["o","o","o","o","o","o","o","o","o","o"])
+        cover_field.insert(x,["x","x","x","x","x","x","x","x","x","x"])
+        for i in range(4): # random assignment for bomb placement, runs 4 times multiplied by the amount of levels completed plus one
+            temp = random.randint(0,9)
+            minesweep_matrix[x].pop((temp))
+            minesweep_matrix[x].insert((temp),"*")
+    for x in range(level  + 1): # counts bombs that are in every single row
+        mine_count = mine_count + minesweep_matrix[x].count("*")
+
+def sweeper_check(column_select,row_select,turn): # checks plot selected
+    if minesweep_matrix[column_select][row_select] == "*" and turn > 0: # returns if bomb stepped on
         return "1A"
-    else:
+    elif minesweep_matrix[column_select][row_select] == "*" and turn == 0: # returns if 1st turn immunity is applied
+        return "1B"
+    else: # checks every single possible area where a bomb could be
         temp = 0
         try:
-            if minesweep_matrix[(plot + 1)] == "*":
+            if minesweep_matrix[column_select][(row_select + 1)]  == "*":
                 temp += 1
         except:
-            return temp
+            pass
         try:
-            if minesweep_matrix[(plot - 1)] == "*":
+            if minesweep_matrix[column_select][(row_select - 1)]  == "*":
                 temp += 1
-            return temp
         except:
-            return temp
+            pass
+        try:
+            if minesweep_matrix[(column_select + 1)][row_select] == "*":
+                temp += 1
+        except:
+            pass
+        try:
+            if minesweep_matrix[(column_select - 1)][row_select] == "*":
+                temp += 1
+        except:
+            pass
+        return temp
+        
 
-
-def field_update(plot,bomb):
-    cover_field.pop(plot)
-    if bomb == "1A":
-        cover_field.insert((plot),"*")
+def field_update(column_select,row_select,bomb):
+    cover_field[column_select].pop(row_select) # gets rid of selected slot
+    if bomb == "1A": # replaces area with bomb
+        cover_field[column_select].insert((row_select),"*")
+    elif bomb == "1B": # replaces area with 1st turn immunity
+        global mine_count
+        cover_field[column_select].insert((row_select),"/")
+        mine_count = mine_count - 1
     else:
-        if bomb == 0:
-            cover_field.insert((plot),"0")
+        if bomb == 0: # replaces with integer
+            cover_field[column_select].insert((row_select),"0")
         else:
-            cover_field.insert((plot),str(bomb))
+            cover_field[column_select].insert((row_select),str(bomb))
 
 def EOR_check(turn): # Checks if you've won or lost
-    if cover_field.count("*") > 0 and turn > 0:
-        print("You landed on a " + colorama.Fore.RED +  "bomb" + colorama.Style.RESET_ALL + "!! You lose!")
-        time.sleep(4)
-        quit()
-    if cover_field.count("x") == mine_count: 
+    global total_x
+    total_x = 0
+    for x in range(level + 1): # checks if any fields have bombs active
+        if cover_field[x].count("*") > 0 and turn > 0:
+            print("You landed on a " + colorama.Fore.RED +  "bomb" + colorama.Style.RESET_ALL + "!! You lose!")
+            time.sleep(4)
+            quit()
+    for x in range(level + 1): # find the total amount of x's left on any turn
+        total_x += cover_field[x].count("x")
+    if total_x == mine_count: # checks if amount of x's left matches total amount of mines 
         os.system('cls')
         UI_elements("sp")
         print("You win! Congratulations!")
@@ -73,18 +103,26 @@ def regenerate(): # runs task to restart game
     os.system('cls')
     global cover_field
     global minesweep_matrix
-    cover_field = ["x","x","x","x","x","x","x","x","x","x"]
-    minesweep_matrix = ["o","o","o","o","o","o","o","o","o","o",]
-    generate()
+    global level
+    cover_field = [["x","x","x","x","x","x","x","x","x","x"] * level]  # resets cover field
+    minesweep_matrix = [[]] # resets minesweep matrix when restarting
+    level += 1
+    generate() 
 
 def UI_elements(type): # basic hud
     if type == "n": # displays current field
         print("===================") 
+        print(f"Mines: {mine_count}")
+        print("Level: " + str(level))
         print("Current Minefield:") 
-        print(cover_field) 
+        for i in range((level + 1)):
+            print(cover_field[i])
         print("===================\n")
     elif type == "sp": # displays final field
         print("===================") 
+        print(f"Mines: {mine_count}")
+        print("Level: " + str(level))
         print("Current Minefield:") 
-        print(minesweep_matrix) 
+        for i in range((level + 1)):
+            print(minesweep_matrix[i]) 
         print("===================\n")
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import colorama
 
 player_data = player.Player()
 selection = 0
-build_ver = "1.2 (indev)"
+build_ver = "1.2"
 
 
 def welcome(): # intro message
@@ -24,7 +24,7 @@ def start_menu(): # basic start CLI
         os.system('cls')
         print("Welcome! Pick your selection: ")
         print("1. Play!")
-        print("2. How to Play")
+        print("2. How to Play (RECOMMENDED FOR NEW PLAYERS)")
         print("3. Quit.")
         selection = int(input("Pick an operation (enter the digit before)"))
         try:
@@ -46,12 +46,16 @@ def start_menu(): # basic start CLI
             os.system('cls')
     gameplay()
         
-def game_info():
+def game_info(): # gives general info on how to play and special symbols
     print("Minesweeper works in a way where you guess spots that are safe, in order to find every possible safe spot.")
     print("This specfic version uses 10 plots, starting at 0, and ending at 9. If a bomb is next to the spot you guessed \nit will be represented by the number of bombs adjacent.")
     print("These are the basic rules of minesweeper!")
+    print("However, this game uses special symbols, here are a list of them:")
+    print("'*' = Bomb")
+    print("(any integer) = Bombs adjacent to space")
+    print("'/' = Would've been a bomb, but 1st turn immunity prevented it.")
     time.sleep(3)
-    temp = input("Ready to be sent back to the main menu? (y/n)")
+    temp = input("\n\nReady to be sent back to the main menu? (y/n)")
     if temp == "y":
         os.system('cls')
         start_menu()
@@ -65,20 +69,21 @@ def gameplay(): # gameplay loop
     generate.generate() # generates minefield
     while True:
         os.system('cls')
-        print(generate.minesweep_matrix)
         generate.UI_elements("n") 
         try: # error
-            selection = int(input("Select a plot number (0-9): "))
-            status = generate.sweeper_check(selection,player_data.turn)
-            generate.field_update(selection,status)
-        except: # handling.
-            print("This is not a valid plot! Enter only numbers between 1 and 10 (inclusive)")
+            column_select = int(input("Select a column number (0 is top, higher is lower on the grid): "))
+            row_select = int(input("Select a plot number (0-9): "))
+            selection = column_select, row_select
+            status = generate.sweeper_check(*selection,player_data.turn)
+            generate.field_update(*selection,status)
+            player_data.turn += 1
+        except: # handling
+            print("This is not a valid location! Please follow the instructions on the inputs!")
             time.sleep(3)
         os.system('cls')
         generate.UI_elements("n")
         generate.EOR_check(player_data.turn)
         time.sleep(1)
-        player_data.turn += 1
 
 welcome()
 start_menu()
